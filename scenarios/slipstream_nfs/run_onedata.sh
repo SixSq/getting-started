1622e2042d737589cb8cfa739ca758ead2330801
Charles Loomis <cal@sixsq.com>
2016-12-20 11:41:41 +0100

    add basic scenario

---
+++ scenarios/slipstream_nfs/run_onedata.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# SlipStream Parameters
+export PROVIDER_TYPE='nfs'
+export ADMIN_USERNAME='admin'
+export ADMIN_PASSWORD=`ss-random`
+ss-set admin-username=${ADMIN_USERNAME}
+ss-set admin-password=${ADMIN_PASSWORD}
+
+# Scenario has to be defined before the source
+SCENARIO_NAME='slipstreamnfs'
+
+source ../../bin/run_onedata.sh 
+
+clean_scenario() {
+	: # pass
+}
+
+main "$@"
