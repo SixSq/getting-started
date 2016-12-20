3de1c32e2a307d03f7f846cac32fb0e0955b9fcd
Charles Loomis <cal@sixsq.com>
2016-12-20 13:30:36 +0100

    fix publishing of usename and passwords

--- scenarios/slipstream_nfs/run_onedata.sh
+++ scenarios/slipstream_nfs/run_onedata.sh
@@ -4,8 +4,8 @@
 export PROVIDER_TYPE='nfs'
 export ADMIN_USERNAME='admin'
 export ADMIN_PASSWORD=`ss-random`
-ss-set admin-username=${ADMIN_USERNAME}
-ss-set admin-password=${ADMIN_PASSWORD}
+ss-set admin-username "${ADMIN_USERNAME}"
+ss-set admin-password "${ADMIN_PASSWORD}"
 
 # Scenario has to be defined before the source
 SCENARIO_NAME='slipstreamnfs'
