7575faa2c67b24aefbddef2f9caa88067c02bc58
Charles Loomis <cal@sixsq.com>
2016-12-20 14:20:31 +0100

    fix parameter names

--- scenarios/slipstream_s3/run_onedata.sh
+++ scenarios/slipstream_s3/run_onedata.sh
@@ -7,10 +7,10 @@ export ADMIN_PASSWORD=`ss-random`
 ss-set admin-username "${ADMIN_USERNAME}"
 ss-set admin-password "${ADMIN_PASSWORD}"
 
-export S3_HOSTNAME `ss-get ss-hostname`
-export S3_ACCESS_KEY `ss-get ss-access-key`
-export S3_SECRET_KEY `ss-get ss-secret-key`
-export S3_BUCKET `ss-get ss-bucket`
+export S3_HOSTNAME `ss-get s3-hostname`
+export S3_ACCESS_KEY `ss-get s3-access-key`
+export S3_SECRET_KEY `ss-get s3-secret-key`
+export S3_BUCKET `ss-get s3-bucket`
 
 # Scenario has to be defined before the source
 SCENARIO_NAME='slipstreams3'
