ebea808567088176b3b081c461dbcc2df6e8823c
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2017-05-01 09:41:53 +0200

    added gluster provider

---
+++ scenarios/slipstream_gluster/run_onedata.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# SlipStream Parameters
+export PROVIDER_TYPE='gluster'
+export ADMIN_USERNAME='admin'
+export ADMIN_PASSWORD=`ss-random`
+ss-set admin-username "${ADMIN_USERNAME}"
+ss-set admin-password "${ADMIN_PASSWORD}"
+
+attach_gluster_volume() {
+    GL_VERSION=`ss-get gl-version`
+    GL_MASTER_VOL=`ss-get gl-master-vol`
+    add-apt-repository -y ppa:gluster/glusterfs-$GL_VERSION
+    apt-get update
+    apt-get install -y glusterfs-client
+    mkdir -p $ONEPROVIDER_DATA_DIR
+    mount -t glusterfs $GL_MASTER_VOL $ONEPROVIDER_DATA_DIR
+}
+
+export ONEPROVIDER_DATA_DIR=/mnt/gluster-onedata
+attach_gluster_volume
+
+# Docker compose project name.
+SCENARIO_NAME='slipstreamgluster'
+
+source ../../bin/run_onedata.sh
+
+clean_scenario() {
+    : # pass
+}
+
+main "$@"
