d1c42270a9d8cb4eed8e9f8ab2b0cecd242b4c38
Eric Le Goff <elegoff@sixsq.com>
2017-06-22 08:26:53 +0200

    Rename input parameter

--- scenarios/slipstream/run_onezone.sh
+++ scenarios/slipstream/run_onezone.sh
@@ -9,10 +9,10 @@ set_docker_image_id
 
 # we should be in the script's directory
 export AUTH_PATH=$(pwd)/auth.config
-HNFEDID_APP_ID=$(ss-get hnfedid-app-id)
+HN_CLIENT_ID=$(ss-get hn-client-id)
 HNFEDID_APP_SECRET=$(ss-get hnfedid-app-secret)
 HNFEDID_URL=$(ss-get hnfedid-url)
-sed -i -e 's|HNFEDID_APP_ID|'$HNFEDID_APP_ID'|' $AUTH_PATH
+sed -i -e 's|HN_CLIENT_ID|'$HN_CLIENT_ID'|' $AUTH_PATH
 sed -i -e 's|HNFEDID_APP_SECRET|'$HNFEDID_APP_SECRET'|' $AUTH_PATH
 sed -i -e 's|HNFEDID_URL|'$HNFEDID_URL'|' $AUTH_PATH
 
