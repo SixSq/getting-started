096b06ea4f734e986f3e0a7a208d199efdbc84fa
Cristovao Cordeiro <cristovao.cordeiro@sixsq.com>
2017-10-05 11:31:27 +0200

    add ioping bmk

--- scenarios/oneclient_fio/run.sh
+++ scenarios/oneclient_fio/run.sh
@@ -13,6 +13,19 @@ rpc.mountd
 rpc.nfsd
 rpc.statd
 
+# add the env for the benchmarks
+cat >>/root/env <<EOF
+IOPING_PATH=$IOPING_PATH
+IOPING_COUNT=${IOPING_COUNT-:5}
+IOPING_DEADLINE=${IOPING_DEADLINE-:60}
+IOPING_SIZE=${IOPING_SIZE-:"1m"}
+CLOUD=$CLOUD
+SERVICE_OFFER=${SERVICE_OFFER-:"service-offer/unknown"}
+NUVLA_KEY=$NUVLA_KEY
+NUVLA_SECRET=$NUVLA_SECRET
+EOF
+
+# add custom line to start cron benchmarks
 cron
 
 oneclient -f "$@"
--- scenarios/oneclient_fio/run_app.sh
+++ scenarios/oneclient_fio/run_app.sh
@@ -1,3 +1,66 @@
 #!/bin/bash
 
-echo `date` >> /root/world
+. /root/env
+
+# run ioping with direct IO (no caching)
+ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -D $IOPING_PATH > /tmp/ioping_output
+
+min=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $7}'`
+min_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $8}'`
+avg=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $11}'`
+avg_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $12}'`
+max=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $15}'`
+max_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $16}'`
+mdev=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $19}'`
+mdev_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $20}'`
+
+cat >>session.json <<EOF
+{
+  "sessionTemplate" : {
+                        "href" : "session-template/api-key",
+                        "key" : "$NUVLA_KEY",
+                        "secret" : "$NUVLA_SECRET"
+                      }
+}
+EOF
+
+curl -H 'content-type: application/json' https://nuv.la/api/session -X POST \
+    -d @session.json --cookie-jar ~/cookies -b ~/cookies -sS
+
+cat >>benchmark.json <<EOF
+{
+  "serviceOffer": {
+      "href": "$SERVICE_OFFER",
+      "connector": {"href": "$CLOUD"}
+  },
+  "credentials": [{"href": "$NUVLA_KEY"}],
+  "ioping:path": "$IOPING_PATH",
+  "ioping:size": "$IOPING_SIZE",
+  "ioping:count": "$IOPING_COUNT",
+  "ioping:avg_score": "$avg",
+  "ioping:avg_units": "$avg_units",
+  "ioping:max_score": "$max",
+  "ioping:max_units": "$max_units",
+  "ioping:min_score": "$min",
+  "ioping:min_units": "$min_units",
+  "ioping:mdev_score": "$mdev",
+  "ioping:mdev_units": "$mdev_units",
+  "ioping:freetext": "ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -D $IOPING_PATH",
+  "acl" : {
+      "owner" : {
+        "principal" : "ADMIN",
+        "type" : "ROLE"
+      },
+      "rules" : [ {
+        "type" : "ROLE",
+        "principal" : "USER",
+        "right" : "VIEW"
+      } ]
+  }
+}
+EOF
+
+curl -X POST --cookie-jar ~/cookies -b ~/cookies -sS \
+    -H "Content-Type: application/json" \
+    https://nuv.la/api/service-benchmark \
+    -d @benchmark.json
