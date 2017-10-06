65be148e68f4ed5dacab4244f455567a0fe634c0
Cristovao Cordeiro <cristovao.cordeiro@sixsq.com>
2017-10-06 10:38:38 +0200

    normalize ioping scores

--- scenarios/oneclient_fio/run_app.sh
+++ scenarios/oneclient_fio/run_app.sh
@@ -3,16 +3,15 @@
 . /root/env
 
 # run ioping with direct IO (no caching)
-ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -D $IOPING_PATH > ioping_output
+ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -B -D $IOPING_PATH > ioping_output
 
-min=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $7}'`
-min_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $8}'`
-avg=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $11}'`
-avg_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $12}'`
-max=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $15}'`
-max_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $16}'`
-mdev=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $19}'`
-mdev_units=`grep "min/avg/max/mdev" ioping_output | awk -F'[/= ]' '{print $20}'`
+duration_usec=`awk -F' ' '{print $2}' ioping_output`
+iops=`awk -F' ' '{print $3}' ioping_output`
+bytes_per_second=`awk -F' ' '{print $4}' ioping_output`
+min_usec=`awk -F' ' '{print $5}' ioping_output`
+avg_usec=`awk -F' ' '{print $6}' ioping_output`
+max_usec=`awk -F' ' '{print $7}' ioping_output`
+mdev_usec=`awk -F' ' '{print $8}' ioping_output`
 
 cat >session.json <<EOF
 {
@@ -37,15 +36,14 @@ cat >benchmark.json <<EOF
   "ioping:path": "$IOPING_PATH",
   "ioping:size": "$IOPING_SIZE",
   "ioping:requests": $IOPING_COUNT,
-  "ioping:avg": $avg,
-  "ioping:avg_units": "$avg_units",
-  "ioping:max": $max,
-  "ioping:max_units": "$max_units",
-  "ioping:min": $min,
-  "ioping:min_units": "$min_units",
-  "ioping:mdev": $mdev,
-  "ioping:mdev_units": "$mdev_units",
-  "ioping:freetext": "ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -D $IOPING_PATH",
+  "ioping:avg_usec": $avg_usec,
+  "ioping:max_usec": $max_usec,
+  "ioping:min_usec": $min_usec,
+  "ioping:mdev_usec": $mdev_usec,
+  "ioping:duration_usec": $duration_usec,
+  "ioping:iops": $iops,
+  "ioping:bytes_per_second": $bytes_per_second,
+  "ioping:freetext": "ioping -c $IOPING_COUNT -w $IOPING_DEADLINE -s $IOPING_SIZE -B -D $IOPING_PATH",
   "acl" : {
       "owner" : {
         "principal" : "ADMIN",
